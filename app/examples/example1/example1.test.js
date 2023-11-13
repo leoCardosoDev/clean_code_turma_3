847cc4cfdb301d92890066ecb6ff709d2c3537fe
leoCardosoDev <leocardosodev@gmail.com>
2023-11-13 08:57:05 -0300

    feat: upadate Example 1

--- app/examples/example1/example1.test.js
+++ app/examples/example1/example1.test.js
@@ -1,20 +1,34 @@
 const example1 = require('./exemple1')
 
 describe('Calculate Ride', () => {
-  test('Deve calcular uma corrida de taxi', function () {
+  test('Deve calcular uma corrida de taxi em dias normais', function () {
     const distance = 1000
     const date = new Date('2021-07-01T10:00:00')
     const price = example1.calculateRide(distance, date)
     expect(price).toBe(2100)
   })
-  
+
   test('Deve calcular uma corrida de taxi a noite', function () {
-    const price = example1.calculateRide(1000, new Date('2021-07-01T23:00:00'))
+    const distance = 1000
+    const date = new Date('2021-07-01T23:00:00')
+    const price = example1.calculateRide(distance, date)
     expect(price).toBe(3900)
   })
-  
+
   test('Deve calcular uma corrida de taxi no Domingo', function () {
-    const price = example1.calculateRide(1000, new Date('2021-07-11T10:00:00'))
+    const distance = 1000
+    const date = new Date('2021-07-11T10:00:00')
+    const price = example1.calculateRide(distance, date)
     expect(price).toBe(2900)
+  })
+
+  test('Deve ser de noite', function() {
+    const isOvernight = example1.isOvernight(new Date('2021-07-01T23:00:00'))
+    expect(isOvernight).toBeTruthy()
+  })
+
+  test('Deve ser de domingo', function() {
+    const isSunday = example1.isSunday(new Date('2021-07-11T23:00:00'))
+    expect(isSunday).toBeTruthy()
   })
 })
--- app/examples/example1/exemple1.js
+++ app/examples/example1/exemple1.js
@@ -1,11 +1,27 @@
-exports.calculateRide = function(distance, date) {
-  if (date.getHours() >= 22) {
-      return distance * 3.90; 
-  } else {
-       if (date.getDay() === 0) {
-           return distance * 2.9;
-       } else {
-           return distance * 2.10;
-       }
-  }
+const OVERNIGTH_RATE = 3.90
+const SUNDAY_RATE = 2.9
+const NORMAL_RATE = 2.10
+
+const isOvernight = function(date) {
+  return date.getHours() >= 22
+}
+
+const isSunday = function(date) {
+  return date.getDay() === 0
+}
+
+const calculateRate = function (distance, rate) {
+  return distance * rate
+}
+
+const calculateRide = function(distance, date) {
+  if (isOvernight(date)) return calculateRate(distance, OVERNIGTH_RATE)
+  if (isSunday(date)) return calculateRate(distance, SUNDAY_RATE)
+  return calculateRate(distance, NORMAL_RATE)
+}
+
+module.exports = {
+  isOvernight,
+  calculateRide,
+  isSunday
 }
