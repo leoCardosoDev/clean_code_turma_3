de0b6fce698c869453e17e51e1e918681678f702
leoCardosoDev <leocardosodev@gmail.com>
2023-11-13 08:23:20 -0300

    test: add 100% coverage for refactoring

--- app/examples/example1/example1.test.js
+++ app/examples/example1/example1.test.js
@@ -1,6 +1,20 @@
 const example1 = require('./exemple1')
 
-test('Deve clacular uma corrida de taxi', function () {
-  const price = example1.calc(1000, new Date('2021-01-01T10:00:00'))
-  expect(price).toBe(2100)
+describe('Calculate Ride', () => {
+  test('Deve calcular uma corrida de taxi', function () {
+    const distance = 1000
+    const date = new Date('2021-07-01T10:00:00')
+    const price = example1.calculateRide(distance, date)
+    expect(price).toBe(2100)
+  })
+  
+  test('Deve calcular uma corrida de taxi a noite', function () {
+    const price = example1.calculateRide(1000, new Date('2021-07-01T23:00:00'))
+    expect(price).toBe(3900)
+  })
+  
+  test('Deve calcular uma corrida de taxi no Domingo', function () {
+    const price = example1.calculateRide(1000, new Date('2021-07-11T10:00:00'))
+    expect(price).toBe(2900)
+  })
 })
