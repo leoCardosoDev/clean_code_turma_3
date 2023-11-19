4102508fc7fb5dff63e3c8186f55ccce196f6c63
leoCardosoDev <leocardosodev@gmail.com>
2023-11-19 06:35:40 -0300

    refactor: change original file

--- app/examples/example2/exemple2.js
+++ app/examples/example2/exemple2.js
@@ -1,62 +1,39 @@
-function validate(str) {
+const CPF_VALID_LENGTH = 11
+const FACTOR_FIRST_VERIFIER_DIGIT = 10
+const FACTOR_SECOND_VERIFIER_DIGIT = 11
 
-	if (str !== null) {
-        if (str !== undefined) {
-            if (str.length >= 11 || str.length <= 14){
+function cleanCpf (cpf) {
+  return cpf.replace(/\D/g,'')
+}
 
-                str=str
-                    .replace('.','')
-                    .replace('.','')
-                    .replace('-','')
-                    .replace(" ","");  
-    
-                if (!str.split("").every(c => c === str[0])) {
-                    try{  
-                        let     d1, d2;  
-                        let     dg1, dg2, rest;  
-                        let     digito;  
-                            let     nDigResult;  
-                        d1 = d2 = 0;  
-                        dg1 = dg2 = rest = 0;  
-                            
-                        for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                            // 	return false;
-                            // } else {
-    
-                                digito = parseInt(str.substring(nCount -1, nCount));  							
-                                d1 = d1 + ( 11 - nCount ) * digito;  
-                
-                                d2 = d2 + ( 12 - nCount ) * digito;  
-                            // }
-                        };  
-                            
-                        rest = (d1 % 11);  
-                
-                        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-                        d2 += 2 * dg1;  
-                        rest = (d2 % 11);  
-                        if (rest < 2)  
-                            dg2 = 0;  
-                        else  
-                            dg2 = 11 - rest;  
-                
-                            let nDigVerific = str.substring(str.length-2, str.length);  
-                        nDigResult = "" + dg1 + "" + dg2;  
-                        return nDigVerific == nDigResult;
-                    }catch (e){  
-                        console.error("Erro !"+e);  
-    
-                        return false;  
-                    }  
-                } else return false
-    
-            }else return false;
-        }
+function areAllDigitsEqual(cpf) {
+  const [firstDigit] = cpf
+  return [...cpf].every(c => c === firstDigit)
+}
+ 
+function calculateDigit (cpf, factor) {
+  let total = 0
+  for (const digit of cpf) {
+    if (factor > 1) total += parseInt(digit) * factor--
+  }
+  const rest = total % 11
+  return (rest < 2) ? 0 : (11 - rest)
+}
 
+function extractVerifierDigit(cpf) {
+  return cpf.slice(9)
+}
 
-	} else return false;
-
+function validate(rawCpf) {
+	if (!rawCpf) return false
+  const cpf = cleanCpf(rawCpf)
+  if (cpf.length !== CPF_VALID_LENGTH) return false
+  if (areAllDigitsEqual(cpf)) return false
+  const firstVerifierDigit = calculateDigit(cpf, FACTOR_FIRST_VERIFIER_DIGIT)
+  const secondVerifierDigit = calculateDigit(cpf, FACTOR_SECOND_VERIFIER_DIGIT)
+  const verifierDigit = extractVerifierDigit(cpf)
+  const calculateVerifierDigit = `${firstVerifierDigit}${secondVerifierDigit}`
+  return verifierDigit === calculateVerifierDigit
 }
 
 module.exports = {
