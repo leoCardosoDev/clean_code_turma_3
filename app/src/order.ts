d3b6752e6c294284b98f9dbaac93b0c790897ee5
leoCardosoDev <leocardosodev@gmail.com>
2023-11-28 06:54:04 -0300

    feat: ensure throws Orde id invalid cpf

---
+++ app/src/order.ts
@@ -0,0 +1,44 @@
+export default class Order {
+  CPF_VALID_LENGTH = 11
+  FACTOR_FIRST_VERIFIER_DIGIT = 10
+  FACTOR_SECOND_VERIFIER_DIGIT = 11
+  
+  constructor(readonly cpf: string) {
+   if(!this.validate(cpf)) throw new Error('Invalid cpf')
+  }
+
+cleanCpf (cpf: string) {
+  return cpf.replace(/\D/g,'')
+}
+
+areAllDigitsEqual(cpf: string) {
+  const [firstDigit] = cpf
+  return [...cpf].every(c => c === firstDigit)
+}
+ 
+calculateDigit (cpf: string, factor: number) {
+  let total = 0
+  for (const digit of cpf) {
+    if (factor > 1) total += parseInt(digit) * factor--
+  }
+  const rest = total % 11
+  return (rest < 2) ? 0 : (11 - rest)
+}
+
+extractVerifierDigit(cpf: string) {
+  return cpf.slice(9)
+}
+
+validate(rawCpf: string) {
+	if (!rawCpf) return false
+  const cpf = this.cleanCpf(rawCpf)
+  if (cpf.length !== this.CPF_VALID_LENGTH) return false
+  if (this.areAllDigitsEqual(cpf)) return false
+  const firstVerifierDigit = this.calculateDigit(cpf, this.FACTOR_FIRST_VERIFIER_DIGIT)
+  const secondVerifierDigit = this.calculateDigit(cpf, this.FACTOR_SECOND_VERIFIER_DIGIT)
+  const verifierDigit = this.extractVerifierDigit(cpf)
+  const calculateVerifierDigit = `${firstVerifierDigit}${secondVerifierDigit}`
+  return verifierDigit === calculateVerifierDigit
+}
+
+}
