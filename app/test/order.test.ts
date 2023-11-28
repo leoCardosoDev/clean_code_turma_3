1a1b5efd300bc30a396057e4106621d72702cc5e
leoCardosoDev <leocardosodev@gmail.com>
2023-11-28 07:52:35 -0300

    test: ensure Order add order items and returns gettotal

--- app/test/order.test.ts
+++ app/test/order.test.ts
@@ -1,10 +1,20 @@
-import Order from '../src/order'
+import Item from '../src/item'
+import Order from "../src/order"
 
-test('Não deve criar um pedido com CPF inválido', () => {
-  expect(() =>  new Order('111.111.111-11')).toThrow(new Error('Invalid cpf'))
+test("Não deve criar um pedido com CPF inválido", () => {
+  expect(() => new Order("111.111.111-11")).toThrow(new Error("Invalid cpf"))
 })
 
-test('Deve criar um pedido', () => {
-  const order = new Order('847.903.332-05')
+test("Deve criar um pedido", () => {
+  const order = new Order("847.903.332-05")
   expect(order).toBeDefined()
+})
+
+test("Deve criar um pedido com 3 itens", () => {
+  const order = new Order("847.903.332-05")
+  order.addItem(new Item(1, "Instrumentos Musicais", "Guitarra", 1000), 1)
+  order.addItem(new Item(2, "Instrumentos Musicais", "Amplificador", 5000), 1)
+  order.addItem(new Item(3, "Instrumentos Musicais", "Guitarra", 30), 3)
+  const total = order.getTotal()
+  expect(total).toBe(6090)
 })
