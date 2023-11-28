d3bbf23002e23c51c3dccf4fe252d4d5b8812efc
leoCardosoDev <leocardosodev@gmail.com>
2023-11-28 07:10:07 -0300

    feat: ensure Order to be defined if correct document is provided

--- app/test/order.test.ts
+++ app/test/order.test.ts
@@ -3,3 +3,8 @@ import Order from '../src/order'
 test('Não deve criar um pedido com CPF inválido', () => {
   expect(() =>  new Order('111.111.111-11')).toThrow(new Error('Invalid cpf'))
 })
+
+test('Deve criar um pedido', () => {
+  const order = new Order('847.903.332-05')
+  expect(order).toBeDefined()
+})
