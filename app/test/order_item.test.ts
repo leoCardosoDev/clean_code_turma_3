99088fcda47c954261dc8b38788c0ff2d274e42b
leoCardosoDev <leocardosodev@gmail.com>
2023-11-28 08:16:39 -0300

    feat: add ordem item test

---
+++ app/test/order_item.test.ts
@@ -0,0 +1,6 @@
+import OrderItem from '../src/order_item'
+
+test('Deve criar um item de pedido', () => {
+  const orderItem = new OrderItem(1, 1000, 2)
+  expect(orderItem.getTotal()).toBe(2000)
+})
