56d133666d0fd73d5cb7368a8b74ba640698e84e
leoCardosoDev <leocardosodev@gmail.com>
2023-11-28 07:52:19 -0300

    feat: ensure Order add order items and returns gettotal

--- app/src/order.ts
+++ app/src/order.ts
@@ -1,49 +1,25 @@
+import Cpf from "./cpf"
+import Item from './item'
+import OrderItem from './order_item'
+
 export default class Order {
-  CPF_VALID_LENGTH = 11;
-  FACTOR_FIRST_VERIFIER_DIGIT = 10;
-  FACTOR_SECOND_VERIFIER_DIGIT = 11;
+  cpf: Cpf
+  orderItems: OrderItem[]
 
-  constructor(readonly cpf: string) {
-    if (!this.validate(cpf)) throw new Error("Invalid cpf");
+  constructor(cpf: string) {
+    this.cpf = new Cpf(cpf)
+    this.orderItems = []
   }
 
-  cleanCpf(cpf: string) {
-    return cpf.replace(/\D/g, "");
+  addItem(item: Item, quantity: number) {
+    this.orderItems.push(new OrderItem(item.idItem, item.price, quantity))
   }
 
-  areAllDigitsEqual(cpf: string) {
-    const [firstDigit] = cpf;
-    return [...cpf].every((c) => c === firstDigit);
-  }
-
-  calculateDigit(cpf: string, factor: number) {
-    let total = 0;
-    for (const digit of cpf) {
-      if (factor > 1) total += parseInt(digit) * factor--;
+  getTotal() {
+    let total = 0
+    for (const orderItem of this.orderItems) {
+      total += orderItem.price * orderItem.quantity
     }
-    const rest = total % 11;
-    return rest < 2 ? 0 : 11 - rest;
-  }
-
-  extractVerifierDigit(cpf: string) {
-    return cpf.slice(9);
-  }
-
-  validate(rawCpf: string) {
-    if (!rawCpf) return false;
-    const cpf = this.cleanCpf(rawCpf);
-    if (cpf.length !== this.CPF_VALID_LENGTH) return false;
-    if (this.areAllDigitsEqual(cpf)) return false;
-    const firstVerifierDigit = this.calculateDigit(
-      cpf,
-      this.FACTOR_FIRST_VERIFIER_DIGIT
-    );
-    const secondVerifierDigit = this.calculateDigit(
-      cpf,
-      this.FACTOR_SECOND_VERIFIER_DIGIT
-    );
-    const verifierDigit = this.extractVerifierDigit(cpf);
-    const calculateVerifierDigit = `${firstVerifierDigit}${secondVerifierDigit}`;
-    return verifierDigit === calculateVerifierDigit;
+    return total
   }
 }
