38cc1e44a2fe4079b66ed0a4223f3880753bf101
leoCardosoDev <leocardosodev@gmail.com>
2023-11-30 17:12:46 -0300

    feat: add test to freight to item

--- app/src/item.ts
+++ app/src/item.ts
@@ -1,3 +1,25 @@
 export default class Item {
-  constructor (readonly idItem: number, readonly category: string, readonly description: string, readonly price: number) {}
+  constructor(
+    readonly idItem: number,
+    readonly category: string,
+    readonly description: string,
+    readonly price: number = 0,
+    readonly width: number = 0,
+    readonly height: number = 0,
+    readonly length: number = 0,
+    readonly weigth: number = 0
+  ) {}
+
+  getVolume() {
+    return this.width / 100 * this.height / 100 * this.length / 100
+  }
+
+  getDensity() {
+    return this.weigth / this.getVolume()
+  }
+
+  getFreight() {
+    const freight = 1000 * this.getVolume() * (this.getDensity() / 100)
+    return (freight < 10) ? 10 : freight
+  }
 }
---
+++ app/test/item.test.ts
@@ -0,0 +1,30 @@
+import Item from '../src/item'
+
+test('Deve criar um item', () => {
+  const item = new Item(1, "Instrumento Musicais", "Guitarra", 1000)
+  expect(item.idItem).toBe(1)
+})
+
+test('Deve criar um item e calcular o volume', () => {
+  const item = new Item(1, "Instrumento Musicais", "Guitarra", 1000, 100, 30, 10)
+  const volume = item.getVolume()
+  expect(volume).toBe(0.03)
+})
+
+test('Deve criar um item e calcular a densidade', () => {
+  const item = new Item(1, "Instrumento Musicais", "Guitarra", 1000, 100, 30, 10, 3)
+  const density = item.getDensity()
+  expect(density).toBe(100)
+})
+
+test('Deve criar um item e calcular o frete', () => {
+  const item = new Item(1, "Instrumento Musicais", "Guitarra", 1000, 100, 30, 10, 3)
+  const freight = item.getFreight()
+  expect(freight).toBe(30)
+})
+
+test('Deve criar um item e calcular o frete minimo', () => {
+  const item = new Item(3, "Instrumento Musicais", "Cabo", 30, 10, 10, 10, 0.9)
+  const freight = item.getFreight()
+  expect(freight).toBe(10)
+})
