687b378a187be8d7bb4556a1ae97ed1ed16e1cfb
leoCardosoDev <leocardosodev@gmail.com>
2023-12-05 08:26:33 -0300

    feat: add database connection adapter

---
+++ app/src/database_connection_adapter.ts
@@ -0,0 +1,12 @@
+import DatabaseConnection from './database_connection'
+import pgp from 'pg-promise'
+
+export default class DatabaseConnectionAdapter implements DatabaseConnection {
+  pgp: any
+  constructor () {
+    this.pgp = pgp()('postgres://postgres:123456@localhost:5432/app')
+  }
+  query(statement: string, params: any) {
+    return this.pgp.query(statement, params)
+  }
+}
