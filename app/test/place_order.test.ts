2ddaff7b4de2406198a36e7615df13a3d00d02f1
leoCardosoDev <leocardosodev@gmail.com>
2023-12-05 07:16:10 -0300

    test: create database connection

--- app/test/place_order.test.ts
+++ app/test/place_order.test.ts
@@ -2,7 +2,7 @@ import ItemRepositoryMemory from '../src/item_repository_memory'
 import OrderRepositoryMermory from '../src/order_repository_memory'
 import PlaceOrder from '../src/place_order'
 
-test('Deve fazer um pedido', () => {
+test('Deve fazer um pedido', async () => {
   const input = {
     cpf: '847.903.332-05',
     orderItems: [
@@ -21,6 +21,6 @@ test('Deve fazer um pedido', () => {
     ]
   }
   const placeOrder = new PlaceOrder(new ItemRepositoryMemory(), new OrderRepositoryMermory())
-  const output = placeOrder.execute(input)
+  const output = await placeOrder.execute(input)
   expect(output.total).toBe(6090)
 })
