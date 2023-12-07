a77e13141429ac10eb1634a2ca5a60b081d1907d
leoCardosoDev <leocardosodev@gmail.com>
2023-12-07 06:44:41 -0300

    feat: ensure add place_order to db connection

--- app/src/item_repository_database.ts
+++ app/src/item_repository_database.ts
@@ -7,8 +7,8 @@ export default class ItemRepositoryDatabase implements ItemRepository {
   constructor (readonly databaseConnection: DatabaseConnection) {}
 
   async findById (idItem: number): Promise<Item> {
-    const itemData = await this.databaseConnection.query('select * from ccca.item where id_item = $1', [idItem])
-    return new Item(itemData.id_item, itemData.category, itemData.description, itemData.price)
+    const [itemData] = await this.databaseConnection.query('select * from cccat3.item where id = $1', [idItem])
+    return new Item(itemData.id, itemData.category, itemData.description, parseFloat(itemData.price))
   }
 
 }
