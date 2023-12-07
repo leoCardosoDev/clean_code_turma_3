8d8539ae6a188c14ec742dfeedb059a4e5f3098b
leoCardosoDev <leocardosodev@gmail.com>
2023-12-07 08:30:26 -0300

    feat: ensure OrderCode create a new code

---
+++ app/src/domain/entity/order_code.ts
@@ -0,0 +1,8 @@
+export default class OrderCode {
+  value: string
+  constructor (readonly date: Date, readonly sequence: number) {
+    const year = date.getFullYear()
+    const sequence8char = `${sequence}`.padStart(8, '0')
+    this.value = `${year}${sequence8char}`
+  }
+}
