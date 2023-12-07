cbf3e9759d4279d8f60694710548848ae30f82e5
leoCardosoDev <leocardosodev@gmail.com>
2023-12-07 08:08:33 -0300

    test: add dto patterns to place order

--- app/test/integration/place_order.test.ts
+++ app/test/integration/place_order.test.ts
@@ -1,12 +1,11 @@
+import PlaceOrderInput from '../../src/application/dto/place_order_input'
 import PlaceOrder from '../../src/application/usecase/place_order'
 import DatabaseConnectionAdapter from '../../src/infra/database/database_connection_adapter'
 import ItemRepositoryDatabase from '../../src/infra/repository/database/item_repository_database'
 import OrderRepositoryMermory from '../../src/infra/repository/memory/order_repository_memory'
 
 test('Deve fazer um pedido', async () => {
-  const input = {
-    cpf: '847.903.332-05',
-    orderItems: [
+  const input = new PlaceOrderInput('847.903.332-05', [
       {
         idItem: 1,
         quantity: 1
@@ -19,8 +18,7 @@ test('Deve fazer um pedido', async () => {
         idItem: 3,
         quantity: 3
       }, 
-    ]
-  }
+    ])
   const placeOrder = new PlaceOrder(new ItemRepositoryDatabase(new DatabaseConnectionAdapter()), new OrderRepositoryMermory())
   const output = await placeOrder.execute(input)
   expect(output.total).toBe(6090)
