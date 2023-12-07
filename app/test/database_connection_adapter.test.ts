4a8644c2a0782e169bffb11758bf03074697786a
leoCardosoDev <leocardosodev@gmail.com>
2023-12-07 07:00:14 -0300

    test: add method close for db connection

--- app/test/database_connection_adapter.test.ts
+++ app/test/database_connection_adapter.test.ts
@@ -1,8 +1,18 @@
 import DatabaseConnectionAdapter from '../src/database_connection_adapter'
 
 describe('Database Connection Adapter', () => {
+  let databaseConnection: DatabaseConnectionAdapter
+
+  beforeAll(async () => {
+    databaseConnection = new DatabaseConnectionAdapter()
+  })
+
+  afterAll(async () => {
+    await databaseConnection.close()
+  })
+
+
   it('Deve criar uma conexão com o banco de dados', async () => {
-    const databaseConnection = new DatabaseConnectionAdapter()
     const items = await databaseConnection.query('SELECT * FROM cccat3.item', [])
     expect(items).toHaveLength(3)
   })
