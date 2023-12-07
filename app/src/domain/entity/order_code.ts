fe3a0d01e97a48cadbaceae0249afcc689ba65be
leoCardosoDev <leocardosodev@gmail.com>
2023-12-07 08:33:54 -0300

    feat: add exception example

--- app/src/domain/entity/order_code.ts
+++ app/src/domain/entity/order_code.ts
@@ -1,6 +1,7 @@
 export default class OrderCode {
   value: string
-  constructor (readonly date: Date, readonly sequence: number) {
+  constructor (private readonly date: Date, private readonly sequence: number) {
+    if (!sequence || !(typeof sequence === 'number')) throw new Error('Invalid parameter')
     const year = date.getFullYear()
     const sequence8char = `${sequence}`.padStart(8, '0')
     this.value = `${year}${sequence8char}`
