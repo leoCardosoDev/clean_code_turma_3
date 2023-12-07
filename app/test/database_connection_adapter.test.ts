dc1f8c2eedc012c008a36ed363300de351b510dd
leoCardosoDev <leocardosodev@gmail.com>
2023-12-07 06:48:13 -0300

    refactor: remove console.log

--- app/test/database_connection_adapter.test.ts
+++ app/test/database_connection_adapter.test.ts
@@ -4,7 +4,6 @@ describe('Database Connection Adapter', () => {
   it('Deve criar uma conexão com o banco de dados', async () => {
     const databaseConnection = new DatabaseConnectionAdapter()
     const items = await databaseConnection.query('SELECT * FROM cccat3.item', [])
-    console.log(items)
     expect(items).toHaveLength(3)
   })
 })
