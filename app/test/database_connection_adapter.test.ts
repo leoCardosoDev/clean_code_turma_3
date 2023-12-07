ace12690aeb00e6028072a229ba459668e4e73f1
leoCardosoDev <leocardosodev@gmail.com>
2023-12-07 06:35:44 -0300

    test: ensure database connection

--- app/test/database_connection_adapter.test.ts
+++ app/test/database_connection_adapter.test.ts
@@ -3,8 +3,8 @@ import DatabaseConnectionAdapter from '../src/database_connection_adapter'
 describe('Database Connection Adapter', () => {
   it('Deve criar uma conexão com o banco de dados', async () => {
     const databaseConnection = new DatabaseConnectionAdapter()
-    const item = await databaseConnection.query('SELECT * FROM cccat3.item', [])
-    console.log(item)
+    const items = await databaseConnection.query('SELECT * FROM cccat3.item', [])
+    expect(items).toHaveLength(3)
   })
 })
 
