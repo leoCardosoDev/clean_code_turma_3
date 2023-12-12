8702619ad0364e6e8413a2a55c5abf2605cedb17
leoCardosoDev <leocardosodev@gmail.com>
2023-12-12 18:07:20 -0300

    test: add coupon repository

--- app/test/integration/place_order.test.ts
+++ app/test/integration/place_order.test.ts
@@ -1,8 +1,23 @@
 import PlaceOrderInput from '../../src/application/dto/place_order_input'
 import PlaceOrder from '../../src/application/usecase/place_order'
 import DatabaseConnectionAdapter from '../../src/infra/database/database_connection_adapter'
+import CouponRepositoryDatabase from '../../src/infra/repository/database/coupon_repository_database'
 import ItemRepositoryDatabase from '../../src/infra/repository/database/item_repository_database'
-import OrderRepositoryMermory from '../../src/infra/repository/memory/order_repository_memory'
+import OrderRepositoryDatabase from '../../src/infra/repository/database/order_repository_database'
+
+let placeOrder: PlaceOrder
+let itemRepository: ItemRepositoryDatabase
+let orderRepository: OrderRepositoryDatabase
+let couponRepository: CouponRepositoryDatabase
+let databaseConnection: DatabaseConnectionAdapter
+
+beforeEach(() => {
+  databaseConnection = new DatabaseConnectionAdapter()
+  itemRepository = new ItemRepositoryDatabase(databaseConnection)
+  orderRepository = new OrderRepositoryDatabase(databaseConnection)
+  couponRepository = new CouponRepositoryDatabase(databaseConnection)
+  placeOrder = new PlaceOrder(itemRepository, orderRepository, couponRepository)
+})
 
 test('Deve fazer um pedido', async () => {
   const input = new PlaceOrderInput('847.903.332-05', [
@@ -18,9 +33,8 @@ test('Deve fazer um pedido', async () => {
         idItem: 3,
         quantity: 3
       }, 
-    ], new Date('2021-03-01'))
-  const placeOrder = new PlaceOrder(new ItemRepositoryDatabase(new DatabaseConnectionAdapter()), new OrderRepositoryMermory())
+    ], new Date('2021-03-01'), 'VALE20')
   const output = await placeOrder.execute(input)
-  expect(output.total).toBe(6090)
+  expect(output.total).toBe(4872)
   expect(output.code).toBe('202100000001')
 })
